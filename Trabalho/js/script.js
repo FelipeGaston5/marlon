623a6bbe1b201ed8867815304a23abb1a7bed888
FelipeGaston5 <Gastonfelipe5@gmail.com>
2024-11-24 02:41:20 -0300

    Dark mode dentro do menu

--- Trabalho/js/script.js
+++ Trabalho/js/script.js
@@ -1,26 +1,3 @@
-// let galeria = 0;
-// const slides =document.querySelectorAll('.carrosel-img');
-
-// function showSlide(i){
-//     galeria += i;
-//     if(galeria>= slides.length) galeria = 0;
-//     if(galeria < 0) galeria = slides.length -1;
-
-//     document.querySelector('.carrosel-slide').style.transform = `translateX(${-galeria * 100}%)`;
-// }
-
-// function moveslide(i){
-//     showSlide(i);
-// }
-
-// setInterval(() => moveslide(1), 3000); 
-
-// document.getElementsByClassName('prev')[0].addEventListener('click', () => moveslide(-1));
-// document.getElementsByClassName('next')[0].addEventListener('click', () => moveslide(1));// Esse codigo faz o carrosel de imagens
-
-
-
-
 document.addEventListener('DOMContentLoaded', ()=> {
     const Refhtml = document.getElementById('Refhtml');
     const menu = document.getElementById('menu');
@@ -29,7 +6,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
         event.stopPropagation();
         menu.classList.toggle('active');
         menu.classList.toggle('hidden');
-    })
+    });
 
 
     document.addEventListener('click', (event) =>{
@@ -37,4 +14,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
             menu.classList.add('hidden');
         }
     })
-})
+});
+
+const trilho = document.querySelector('.trilho');
+const body = document.body;
+const button = document.querySelector('#button');
+
+trilho.addEventListener('click', () => {
+    body.classList.toggle('dark');
+});
+
+button.addEventListener('click', (event) =>{
+    event.stopPropagation();
+    body.classList.toggle('dark');
+});
