6b43da95ab6f96119d97672345fa5ef06898ad0e
FelipeGaston5 <Gastonfelipe5@gmail.com>
2024-11-25 18:49:45 -0300

    agora

--- Trabalho/js/script.js
+++ Trabalho/js/script.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    const Refhtml = document.getElementById('Refhtml');
-    const menu = document.getElementById('menu');
+// document.addEventListener('DOMContentLoaded', ()=> {
+//     const Refhtml = document.getElementById('Refhtml');
+//     const menu = document.getElementById('menu');
 
-    Refhtml.addEventListener('click', (event) =>{
-        event.stopPropagation();
-        menu.classList.toggle('active');
-        menu.classList.toggle('hidden');
-    });
+//     Refhtml.addEventListener('click', (event) =>{
+//         event.stopPropagation();
+//         menu.classList.toggle('active');
+//         menu.classList.toggle('hidden');
+//     });
 
 
-    document.addEventListener('click', (event) =>{
-        if(!menu.contains(event.target) && !Refhtml.contains(event.target)){
-            menu.classList.add('hidden');
-        }
-    })
-});
+//     document.addEventListener('click', (event) =>{
+//         if(!menu.contains(event.target) && !Refhtml.contains(event.target)){
+//             menu.classList.add('hidden');
+//         }
+//     })
+// });
 
 const trilho = document.querySelector('.trilho');
 const body = document.body;
@@ -28,3 +28,44 @@ button.addEventListener('click', (event) =>{
     event.stopPropagation();
     body.classList.toggle('dark');
 });
+
+
+// menu Hamburguer
+        const toggleButton = document.getElementById('menu-toggle');
+        const menuDropdown = document.getElementById('menu-dropdown');
+
+        toggleButton.addEventListener('click', () => {
+            // Alterna as classes para animar e abrir o menu
+            toggleButton.classList.toggle('active');
+            menuDropdown.classList.toggle('open');
+            
+        });
+        document.addEventListener('click', (event) => {
+        const isClickInsideMenu = menuDropdown.contains(event.target) || toggleButton.contains(event.target);
+        
+        if (!isClickInsideMenu) {
+        toggleButton.classList.remove('active');
+        menuDropdown.classList.remove('open');
+    }})
+    
+// Aumentar e Diminuir fonte
+const root = document.documentElement;
+        const padraofonte= 16;
+        let Fonte = padraofonte;
+
+        function aumentarfonte(){
+            if (Fonte < 24){    
+            Fonte += 2;
+            root.style.setProperty('--base-fonte', `${Fonte}px`);
+        }
+    }
+        function Diminuirfonte() {
+            if (Fonte > 13){ 
+                Fonte -= 2;
+                root.style.setProperty('--base-fonte', `${Fonte}px`);
+            }
+        }
+        document.getElementById('aumentarfonte').addEventListener('click', aumentarfonte);
+        document.getElementById('Diminuirfonte').addEventListener('click', Diminuirfonte);
+
+        
